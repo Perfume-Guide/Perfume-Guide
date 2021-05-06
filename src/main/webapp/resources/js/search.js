9fdf18606af507c4e92de5a4ea1232668ce22414
JAEHYUK-lab <afentis508@gmail.com>
2021-05-06 15:02:06 +0900

    remove

--- src/main/webapp/resources/js/search.js
+++
@@ -1,8 +0,0 @@
-function openNav() {
-    document.getElementById("mySearch").style.top = "0px";
-    document.getElementById("mySearch").style.opacity = "1";
-  }
-  function closeNav() {
-    document.getElementById("mySearch").style.top = "-350px";
-    document.getElementById("mySearch").style.opacity = "0";
-  }
